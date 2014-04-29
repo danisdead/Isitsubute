ab2412777572b057f6ba65c5a618d1ab5bb3a835
Ilse <ilse@ccdigital.mx>
2014-04-29 13:08:57 -0500

    Estilos flechas anterior/siguiente

--- js/activacion.js
+++ js/activacion.js
@@ -4,6 +4,13 @@ $(document).ready(function(){
 	$(document).find('div#slide2').css({'height' : window.innerHeight/*, 'width' : window.innerWidth*/});
 	$(document).find('div#slide3').css({'height' : window.innerHeight/*, 'width' : window.innerWidth*/});
 
+	/*Acomoda las flechas de siguiente y anterior al inicio y al final de la pantalla*/
+	$(document).find('#sig_s1').css({'top' : window.innerHeight - (window.innerHeight * 1.15)});
+	//$(document).find('#sig_s2').css({'top' : 0});
+	//$(document).find('#ant_s1').css({'top' : 0});
+	$(document).find('#ant_s2').css({'top' : window.innerHeight - (window.innerHeight * 1.1)});
+	$(document).find('#ant_s3').css({'top' : window.innerHeight - (window.innerHeight * 1.07)});
+
 	var overActivacion = $('<div>').attr('id', 'over_act');
 	overActivacion.css({'background-color' : 'red', 'height' : '200px', 'background' : 'url("img/TOOLTIP_PATTERN2.png") repeat-x', 'position': 'fixed', 'top' : '0px', 'margin' : '10% 0 0 -40%', 'z-index' : '2',  'left' : '50%', 'display' : 'none'});
 	
